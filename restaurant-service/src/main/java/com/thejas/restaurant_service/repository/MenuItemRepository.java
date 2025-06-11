bb4b622ccb0f320981927dd15e9d6b3bd2382ebc
thejas01 <thejasgs19@gmail.com>
2025-06-11 10:03:50 +0530

    chors: controller modification

--- restaurant-service/src/main/java/com/thejas/restaurant_service/repository/MenuItemRepository.java
+++ restaurant-service/src/main/java/com/thejas/restaurant_service/repository/MenuItemRepository.java
@@ -1,42 +1,11 @@
-package com.thejas.restaurant_service.controller;
+package com.thejas.restaurant_service.repository;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thejas.restaurant_service.model.MenuItem;
-import com.thejas.restaurant_service.model.Restaurant;
-import com.thejas.restaurant_service.service.MenuItemService;
-import com.thejas.restaurant_service.service.RestaurantService;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/restaurants/{restaurantId}/menu")
-@CrossOrigin("*")
-public class MenuItemController {
-
-    private final MenuItemService itemService;
-    private final RestaurantService restaurantService;
-
-    public MenuItemController(MenuItemService itemService, RestaurantService restaurantService) {
-        this.itemService = itemService;
-        this.restaurantService = restaurantService;
-    }
-
-    @PostMapping
-    public ResponseEntity<MenuItem> addItem(
-            @PathVariable Long restaurantId,
-            @RequestBody MenuItem item
-    ) {
-        Restaurant r = restaurantService.getById(restaurantId);
-        if (r == null) return ResponseEntity.notFound().build();
-
-        item.setRestaurant(r);
-        return ResponseEntity.ok(itemService.save(item));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<MenuItem>> getMenu(@PathVariable Long restaurantId) {
-        return ResponseEntity.ok(itemService.getByRestaurant(restaurantId));
-    }
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+    List<MenuItem> findByRestaurantId(Long restaurantId);
 }
